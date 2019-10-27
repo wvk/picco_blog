efa846d273d7add70e5442708cd585f965777fa9
Willem van Kerkhof <wvk@consolving.de>
2019-10-27 17:14:25 +0100

    Jump back to wherever the comment form was displayed after creating a comment

--- app/controllers/picco_blog/application_controller.rb
+++ app/controllers/picco_blog/application_controller.rb
@@ -1,5 +1,19 @@
 module PiccoBlog
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ActionController::Base
     layout PiccoBlog.layout
+
+    before_action :authenticate_user!
+
+    protected
+
+    helper_method :current_url, :back_or_default
+
+    def current_url
+      request.url
+    end
+
+    def back_or_default(default_path)
+      params[:referrer] || default_path
+    end
   end
 end
