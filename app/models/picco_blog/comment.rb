b2490209367b8a9fd4c48725ce8ba76799dc1e0e
Willem van Kerkhof <wvk@consolving.de>
2021-03-11 08:05:21 +0100

    minimum text length and word count for comments

--- app/models/picco_blog/comment.rb
+++ app/models/picco_blog/comment.rb
@@ -3,5 +3,16 @@ module PiccoBlog
     belongs_to :post
 
     acts_as_nested_set
+
+    validate :more_than_one_word
+
+
+    protected
+
+    def more_than_one_word
+      if self.text.length < 10 or self.text.split(/\s+/).size < 2
+        self.errors.add :text, "ein Bisschen mehr darf's schon sein!"
+      end
+    end
   end
 end
